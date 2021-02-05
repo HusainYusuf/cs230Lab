f7cb0ea9e000fc9934d6378bb6824d78f4e149f6
HusainYusuf <hy0025@mix.wvu.edu>
2021-02-04 19:54:51 -0500

    Lab2 about page

---
+++ index.php
@@ -0,0 +1,55 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us </h1>
+        <p>For Weebs By Weebs</p>
+    </div>
+
+<div class="row">
+<div class="column">
+    <div class="card">
+        <img src="images/4.jpeg">
+        <div class="container">
+            <h2>Primary Writer</h2>
+            <p>Name: Manaf Alajmi</p>
+            <p>Favorite Series: Sword Art Online</p>
+            <p>Contact: animethighlover123@gmail.com</p>
+        </div>
+    </div>
+</div>
+<div class="column">
+    <div class="card">
+        <img src="images/1.jpg">
+        <div class="container">
+            <h2>Ultimate Degenrate/Writer</h2>
+            <p>Name: The Anime Zone</p>
+            <p>Favorite Series: Full Metal Alchemist Brotherhood</p>
+            <p>Contact: giggUK@gmail.com</p>
+        </div>
+    </div>
+</div>
+<div class="column">
+    <div class="card">
+        <img src="images/2.jpeg">
+        <div class="container">
+            <h2>Professional Anime Watcher/Writer</h2>
+            <p>Name: The *Henti* Man/Writer</p>
+            <p>Favorite Series: Kiss x Sis</p>
+            <p>Contact: animeman@gmail.com</p>
+        </div>
+    </div>
+</div>
+<div class="column">
+    <div class="card">
+        <img src="images/3.jpg">
+        <div class="container">
+            <h2>Normie/Writer</h2>
+            <p>Name: CdawgVa</p>
+            <p>Favorite Series: Jojos Bizarre Adventures</p>
+            <p>Contact: monkeybrain@gmail.com</p>
+        </div>
+    </div>
+</div>
+</div>
+</body>
